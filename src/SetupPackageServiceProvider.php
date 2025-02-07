11979d4650121e1cc2e416c0663792a56b46d194
Sanne van Leeuwen <88337945+Leeuwenkasteel@users.noreply.github.com>
2025-02-07 11:40:29 +0100

    Update SetupPackageServiceProvider.php

--- src/SetupPackageServiceProvider.php
+++ src/SetupPackageServiceProvider.php
@@ -11,11 +11,37 @@ class SetupPackageServiceProvider extends ServiceProvider{
   }
 
   public function boot(): void{
-
+	$this->loadOptionalPackages();
 	if ($this->app->runningInConsole()) {
       $this->commands([
           ShowOptionalPackages::class
       ]);
     }
   }
+  
+  private function loadOptionalPackages()
+    {
+        $packages = [
+            'leeuwenkasteel\\analyticus\\AnalyticusServiceProvider',
+            'leeuwenkasteel\\auth\\AuthServiceProvider',
+            'leeuwenkasteel\\contact\\ContactServiceProvider',
+            'leeuwenkasteel\\domains\\DomainsServiceProvider',
+		'leeuwenkasteel\\domains\\ErrorLoggerServiceProvider',
+            'leeuwenkasteel\\languages\\LanguagesServiceProvider',
+            'leeuwenkasteel\\media\\MediaServiceProvider',
+		'leeuwenkasteel\\domains\\MenuServiceProvider',
+            'leeuwenkasteel\\schema\\SchemaServiceProvider',
+            'leeuwenkasteel\\seo\\SeoServiceProvider',
+            'leeuwenkasteel\\scholen\\ScholenServiceProvider',
+            'leeuwenkasteel\\templates\\TemplatesServiceProvider',
+		'leeuwenkasteel\\domains\\UnderConstuctionServiceProvider',
+            'leeuwenkasteel\\webshop\\WebshopServiceProvider'
+        ];
+
+        foreach ($packages as $provider) {
+            if (class_exists($provider)) {
+                $this->app->register($provider);
+            }
+        }
+    }
 }
