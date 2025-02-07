c206864c45f594b067d0d98735fe133c8aec13ae
Leeuwenkasteel <sanne@leeuwenkasteel.nl>
2025-02-07 11:36:50 +0100

    update

--- src/SetupPackageServiceProvider.php
+++ src/SetupPackageServiceProvider.php
@@ -11,11 +11,34 @@ class SetupPackageServiceProvider extends ServiceProvider{
   }
 
   public function boot(): void{
-
+	$this->loadOptionalPackages();
 	if ($this->app->runningInConsole()) {
       $this->commands([
           ShowOptionalPackages::class
       ]);
     }
   }
+  
+  private function loadOptionalPackages()
+    {
+        $packages = [
+            'leeuwenkasteel\\analyticus\\AnalyticusServiceProvider',
+            'leeuwenkasteel\\auth\\AuthServiceProvider',
+            'leeuwenkasteel\\contact\\ContactServiceProvider',
+            'leeuwenkasteel\\domains\\DomainsServiceProvider',
+            'leeuwenkasteel\\languages\\LanguagesServiceProvider',
+            'leeuwenkasteel\\media\\MediaServiceProvider',
+            'leeuwenkasteel\\schema\\SchemaServiceProvider',
+            'leeuwenkasteel\\seo\\SeoServiceProvider',
+            'leeuwenkasteel\\scholen\\ScholenServiceProvider',
+            'leeuwenkasteel\\templates\\TemplatesServiceProvider',
+            'leeuwenkasteel\\webshop\\WebshopServiceProvider'
+        ];
+
+        foreach ($packages as $provider) {
+            if (class_exists($provider)) {
+                $this->app->register($provider);
+            }
+        }
+    }
 }
