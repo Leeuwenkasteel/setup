ddcadcb2f672d0cc46dc463b224aeed9cc6f0908
Leeuwenkasteel <sanne@leeuwenkasteel.nl>
2025-02-11 14:54:15 +0100

    update

--- src/SetupPackageServiceProvider.php
+++ src/SetupPackageServiceProvider.php
@@ -13,15 +13,12 @@ class SetupPackageServiceProvider extends ServiceProvider{
   }
 
   public function boot(): void{
-<<<<<<< HEAD
 	  $this->loadViewsFrom(__DIR__.'/../resources/views', 'setup');
 		$this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 		
 		Livewire::component('setup::package', PackageManager::class);
 		$this->mergeConfigFrom(__DIR__.'/../config/setup.php', 'config-setup');
 	
-=======
->>>>>>> d8217ff83b6e5707ceaa3f5e7a07d58ded025151
 	$this->loadOptionalPackages();
 	if ($this->app->runningInConsole()) {
       $this->commands([
@@ -37,23 +34,17 @@ class SetupPackageServiceProvider extends ServiceProvider{
             'leeuwenkasteel\\auth\\AuthServiceProvider',
             'leeuwenkasteel\\contact\\ContactServiceProvider',
             'leeuwenkasteel\\domains\\DomainsServiceProvider',
-<<<<<<< HEAD
             'leeuwenkasteel\\languages\\LanguagesServiceProvider',
             'leeuwenkasteel\\media\\MediaServiceProvider',
-=======
 		'leeuwenkasteel\\domains\\ErrorLoggerServiceProvider',
             'leeuwenkasteel\\languages\\LanguagesServiceProvider',
             'leeuwenkasteel\\media\\MediaServiceProvider',
 		'leeuwenkasteel\\domains\\MenuServiceProvider',
->>>>>>> d8217ff83b6e5707ceaa3f5e7a07d58ded025151
             'leeuwenkasteel\\schema\\SchemaServiceProvider',
             'leeuwenkasteel\\seo\\SeoServiceProvider',
             'leeuwenkasteel\\scholen\\ScholenServiceProvider',
             'leeuwenkasteel\\templates\\TemplatesServiceProvider',
-<<<<<<< HEAD
-=======
 		'leeuwenkasteel\\domains\\UnderConstuctionServiceProvider',
->>>>>>> d8217ff83b6e5707ceaa3f5e7a07d58ded025151
             'leeuwenkasteel\\webshop\\WebshopServiceProvider'
         ];
 
@@ -63,8 +54,6 @@ class SetupPackageServiceProvider extends ServiceProvider{
             }
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> d8217ff83b6e5707ceaa3f5e7a07d58ded025151
+
+
